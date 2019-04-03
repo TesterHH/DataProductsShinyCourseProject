18da2356a1b6b0b16cce1e4a47f6b500d863eace
TesterHH <egorelov@physnet.uni-hamburg.de>
2019-04-03 14:59:25 +0200

    index corrected

--- server.R
+++ server.R
@@ -1,17 +1,31 @@
-library(shiny)
-library(UsingR)
-data(galton)
+library(ggplot2)
+library(tidyr)
+data(mtcars)
 
-shinyServer(
-  function(input, output) {
-    output$newHist <- renderPlot({
-      hist(galton$child, xlab='child height', col='lightblue',main='Histogram')
-      mu <- input$mu
-      lines(c(mu, mu), c(0, 200),col="red",lwd=5)
-      mse <- mean((galton$child - mu)^2)
-      text(63, 150, paste("mu = ", mu))
-      text(63, 140, paste("MSE = ", round(mse, 2)))
-      })
+shinyServer(  
+  function(input, output) { 
     
-  }
+    mtcars$am<-as.factor(as.character(mtcars$am))
+    levels(mtcars$am)[levels(mtcars$am)=="0"] <- "Automatic"
+    levels(mtcars$am)[levels(mtcars$am)=="1"] <- "Manual"
+    fit0<-lm(mpg~am, data=mtcars)
+    tabble<-as.data.frame(cbind(c("1", "2", "3", "4", "5", "6",  "7", "8", "9", "10", "11"),
+                  c("mpg", "cyl", "disp", "hp", "drat", "wt",  "qsec", "vs", "am", "gear", "carb"),
+                  c("Miles/(US) gallon", "Number of cylinders", "Displacement (cu.in.)", "Gross horsepower", "Rear axle ratio", "Weight (1000 lbs)",  "1/4 mile time", "Engine 0/1=V/I", "Transmission type", "Number of forward gears", "Number of carburetors")))
+    names(tabble)<-c("column", "variable","explanation")
+    output$varrs <- renderTable({
+    tabble})
+   
+    output$myPlot <- renderPlot({
+      ff<-as.formula(paste("mpg~am+",input$secondVar, collapse=""))
+      g<-ggplot(mtcars, aes(get(input$secondVar), mpg, shape=am, colour=am, fill=am))+geom_point()
+      g+labs(x=input$secondVar)+geom_smooth(method="lm")
+       })
+
+    output$myText<-renderText({
+      ff<-as.formula(paste("mpg~am+",input$secondVar, collapse=""))
+      fit1<-lm(ff, data=mtcars)
+      paste(as.character(anova(fit0,fit1)$Pr[2]))
+    })
+    }
 )
--- ui.R
+++ ui.R
@@ -1,10 +1,34 @@
-library(shiny)
-shinyUI(pageWithSidebar(
-  headerPanel("Example plot"),
-  sidebarPanel(
-    sliderInput('mu', 'Guess at the mean',value = 70, min = 62, max = 74, step = 0.05,)
-  ),
-  mainPanel(
-    plotOutput('newHist')
+shinyUI(pageWithSidebar(  
+  headerPanel("Analysis of cars fuel consumption in mtcars data set"),  
+  sidebarPanel(    
+    radioButtons("secondVar", "Additional predictor",
+                 c("Number of cylinders" = "cyl",
+                   "Displacement (cu.in.)" = "disp",
+                   "Gross horsepower" = "hp",
+                   "Rear axle ratio" = "drat",
+                   "Weight (1000 lbs)" = "wt",
+                   "1/4 mile time" = "qsec",
+                   "Engine (0 = V-shaped, 1 = straight)" = "vs",
+                   "Number of forward gears" = "gear",
+                   "Number of carburetors" = "carb"
+                  )),
+    h5("The mtcars data set is extracted from the 1974 Motor Trend US magazine, and comprises fuel consumption and 10 aspects of automobile design and performance for 32 automobiles (1973–74 models).
+The data set includes data for 32 cars with 11 variables:"),
+tableOutput("varrs")),
+  mainPanel(    
+    plotOutput('myPlot'),
+    p('p-value, associated with selected additional predictor:'),
+    textOutput('myText'),
+    h5("Studiing the mtcars dataset using the linear fit model we
+illustrate, which variables of the data set should be included into the model
+       in order to explain fuel consumption differrence between cars with automatic and manual transmittion.
+       Selecting attitional predictor from the list using radiobuttons,
+build additional linear fit1 with two predictors, compare it to basis fit0
+with single predictor (type of transmission), and plot obtained linear fit versus
+additional predictor. The p-value, associated with selected additional predictor
+is also reported, giving an information of significance of the selected predictor 
+to explanation of fuel consumption difference of cars from the data set.
+       ")
   )
 ))
+
